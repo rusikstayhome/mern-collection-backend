fcf8f78f48d8136d2035ee0e9f8644f216405426
rusikstayhome <alenrus32@gmail.com>
2022-10-07 10:39:30 +0300

    added .env file

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from "express";
+import jwt from 'jsonwebtoken'
+
 
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.bffceli.mongodb.net/collection?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASS}@cluster0.bffceli.mongodb.net/collection?retryWrites=true&w=majority`)
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
 
@@ -12,6 +17,22 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello world!")
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: "lol"
+    },
+        'ilovenika',
+    );
+
+    res.json({
+        succes: true,
+        token
+    })
 })
 
 app.listen(3001, (err) => {
